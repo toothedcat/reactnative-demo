1917a59f9b761a4d7fd66cad6d2b54febf30252b
toothedcat <toothedcat@163.com>
2017-09-19 10:50:34 +0800

    TextInput Demo

--- FirstProject/App.js
+++ FirstProject/App.js
@@ -3,12 +3,14 @@ import { StackNavigator } from 'react-navigation';
 import Home from './app/Home';
 import StyleDemo from './app/StyleDemo';
 import { SizeFixedDemo, SizeFlexDemo } from './app/SizeDemo';
+import InputDemo from './app/InputDemo';
 
 const App = StackNavigator({
     Home: { screen: Home },
     StyleDemo: { screen: StyleDemo },
     SizeFixedDemo: { screen: SizeFixedDemo },
-    SizeFlexDemo: { screen: SizeFlexDemo }
+    SizeFlexDemo: { screen: SizeFlexDemo },
+    InputDemo:{screen:InputDemo}
 });
 
 export default App;
